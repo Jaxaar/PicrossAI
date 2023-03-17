3f4cf6cf1dc598e81f62fe3b73b36258946b7740
Jaxaar <jax1664-cs@yahoo.com>
2023-03-17 16:32:04 -0400

    Added AC3 Test
    AC3 Breaking but

--- src/test/java/PicrossWorldTest.java
+++ src/test/java/PicrossWorldTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class PicrossWorldTest {
 
     @Test
-    void AC3Forwarding() {
+    void AC3ForwardingTest() {
+        PicrossPuzzle pz = new PicrossPuzzle(10,10);
+        PicrossWorld pw = new PicrossWorld(pz);
+        System.out.println(pw);
+        pw.AC3Forwarding();
+        System.out.println(pw);
     }
 }
