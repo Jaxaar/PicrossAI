da66a682123039946d4a1b448e0ba4b51d6ed6b7
Jaxaar <jax1664-cs@yahoo.com>
2023-03-17 21:24:41 -0400

    Update PicrossWorldTest.java

--- src/test/java/PicrossWorldTest.java
+++ src/test/java/PicrossWorldTest.java
@@ -8,25 +8,35 @@ class PicrossWorldTest {
 
     @Test
     void AC3ForwardingTest() throws FileNotFoundException {
-        PicrossPuzzle pz = new PicrossPuzzle(10,10);
+        int size = 10;
+        PicrossPuzzle pz = new PicrossPuzzle(size,size);
 //        PicrossPuzzle pz = new PicrossPuzzle("picross10x10.csv");
         PicrossWorld pw = new PicrossWorld(pz);
 //        System.out.println(pw.toString());
         PicrossWorld preAC3 = new PicrossWorld(pw);
         pw.AC3Forwarding();
 
-        for(int i=0; i<10; i++)
+        for(int i=0; i<size -1 ; i++)
         {
             System.out.println();
         }
 
 //        System.out.println(pw.toString());
 
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < size; i++){
             System.out.println("PW row: " + i);
-            System.out.println(pw.getRowDomains().get(i).getDomSize());
+            int x = pw.getRowDomains().get(i).getDomSize();
+            System.out.println(x);
             System.out.println("PreAC3 row: " + i);
-            System.out.println(preAC3.getRowDomains().get(i).getDomSize());
+            int y = pw.getRowDomains().get(i).getDomSize();
+            if(x != y){
+                System.out.println(x);
+                assert true;
+            }
+            else {
+                System.out.println(y);
+            }
         }
+        assert false;
     }
 }
